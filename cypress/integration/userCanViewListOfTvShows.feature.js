945fac499fec8bed6ac4eceb4a91a29b18dc5ee5
designerofthing <designerofthings@gmail.com>
2020-06-26 13:56:17 +0200

    adds implementation and some styling

--- cypress/integration/userCanViewListOfTvShows.feature.js
+++ cypress/integration/userCanViewListOfTvShows.feature.js
@@ -15,7 +15,7 @@ describe("User can view a list of TV shows", () => {
 
   it("showns an image inside the header", () => {
     cy.get("#header").within(() => {
-      cy.get("#logo").find("img").should("be.visible");
+      cy.get("#logo").find("img#logo").should("be.visible");
     });
   });
   it("shows tv shows images", () => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
+import axios from 'axios';
 
 const App = () => {
+const [ series, setSeries ] = useState([])
+
+  useEffect(() => {
+    const getShows = async () =>{
+      try {
+        const response = await axios.get('https://content.viaplay.se/pc-se/serier/samtliga')
+             setSeries(response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products'])
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getShows();
+  }, [])
+
+    let tvShow = series.map((show) => {
+      return(
+        <div className='display-show' >
+      <img src={show.content.images.landscape.url} alt={show.content.series.title} /> 
+      </div>
+        )
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+    <div >
+      <header id='header'>
+        <img id="logo" src='https://kundservice.viaplay.se/wp-content/themes/viaplaycs/assets/dist/images/viaplay_white.svg'  alt="logo" />
       </header>
+    <div id='main-container'>
+      {tvShow}
+    
+    </div>
+    <footer id='footer' />
     </div>
   );
 }
